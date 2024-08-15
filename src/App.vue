8545fe849dc344cc045682d989e9776904585f38
VYKETETRORP <lesleygs56@gamil.com>
2024-08-15 09:54:15 +0700

    paint

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,71 @@
 <template>
-  app
-  <HelloWorld />
+  <canvas ref="canvas" height="500" width="500" @mousedown="onMouseDown"  @mousemove="onMouseMove" @mouseup="onMouseUp"></canvas>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from "./components/HelloWorld.vue"
+import { ref,onMounted } from "vue";
+import { defineComponent } from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
 export default defineComponent({
-  name: 'App',
-  components : {
-    HelloWorld
+  name: "App",
+  components: {
+    HelloWorld,
+  },
+  setup() {
+    const canvas = ref<HTMLCanvasElement>();
+    let isMouseClicked = false
 
-  }
+    const DrawCircle = (x:number, y:number) => {
+      let ctx = canvas.value?.getContext("2d");
+      if (ctx) {
+        ctx.beginPath();
+        ctx.arc(x, y, 20, 0, 2 * Math.PI);
+        ctx.fill();
+      }
+    };
+    const onMouseDown = (e:MouseEvent)=>{
+      isMouseClicked = true
+      let x = e.offsetX
+      let y = e.offsetY
+      DrawCircle(x,y);
+
+    }
+    const onMouseMove = (e:MouseEvent)=>{
+      let x = e.offsetX
+      let y = e.offsetY
+      DrawCircle(x,y);
+
+    }
+    const onMouseUp = ()=>{
+
+    }
+
+    
+    onMounted(()=>{
+      DrawCircle(200,200)
+    })
+
+    return {
+      canvas,
+      onMouseDown,
+      onMouseMove,
+      onMouseUp,
+    };
+    
+  },
   
 });
 </script>
 
-<style scoped>
-
+<style>
+body {
+  margin: 0;
+  display: flex;
+  justify-content: center;
+  min-height: 100vh;
+  align-items: center;
+}
+canvas {
+  border: 1px solid black;
+}
 </style>
